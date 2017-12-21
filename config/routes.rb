88bfe2642e820e06eef4b4e6c9097499072155a0
Stu Franks <stu.j.franks@gmail.com>
2017-12-21 15:13:15 +0000

    Set homepage to leaderboard show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :players
   resource :leaderboards, :as => :leaderboard, :path => :leaderboard
+  root 'leaderboards#show'
+
 end
